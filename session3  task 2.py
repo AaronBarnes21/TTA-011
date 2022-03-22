68d1666d15200fa954b717641fe0c6eba417eb27
AaronBarnes21 <98524443+AaronBarnes21@users.noreply.github.com>
2022-03-22 07:35:59 +0000

    Update session3  task 2.py

--- session3  task 2.py
+++ session3  task 2.py
@@ -20,7 +20,7 @@ elif avg >= 68 and avg <= 72:
     print("your grade is C")
 elif avg >= 52 and avg <= 68:
     print("your grade is D")
-elif avg >= 52 and avg <= 0:
+elif avg >= 0 and avg <=52:
     print("your grade is E")
 else:
   
