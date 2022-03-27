84372f9c1c14d0c5583b08af0b59f457f42c9634
AaronBarnes21 <98524443+AaronBarnes21@users.noreply.github.com>
2022-03-27 14:46:56 +0100

    Add files via upload

---
+++ Fibonacci numbers.r
@@ -0,0 +1,8 @@
+
+Fibonacci < numeric(10)
+Fibonacci[1] <- Fibonacci[2]<- 1
+for(i in 2 : 10)Fibonacci[1]<-1 Fibonacci[i - 2] + Fibonacci[i-1]
+int("first 10 fibonacci numbers")
+int(fibonacci)
+
+explain the fibonacci numbers
