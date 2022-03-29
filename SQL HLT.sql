ce3dfd5dcd0e02c19db90c0c3f5a9bac185fc504
AaronBarnes21 <98524443+AaronBarnes21@users.noreply.github.com>
2022-03-29 01:28:09 +0100

    Add files via upload

---
+++ SQL HLT.sql
@@ -0,0 +1,13 @@
+show databases;
+
+use headphones
+
+create table headphone_details(
+primary key (customers_ID),
+customer_ID varchar(3) not null,
+wireless varchar(20)not null,
+release_date date,
+price decimal (9,2) not null,
+model varchar not null
+);
+
