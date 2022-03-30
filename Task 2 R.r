d0def007cc7aa0473e141d38c839bf6af5ace772
AaronBarnes21 <98524443+AaronBarnes21@users.noreply.github.com>
2022-03-30 17:32:36 +0100

    Add files via upload

---
+++ Task 2 R.r
@@ -0,0 +1,8 @@
+employees= data.frames(Name=c)("Natalia S","Mael T","Sophie B","Clark S","Timothy H"),
+                    Gender=c("M","F","M","M","F "),
+                    age=c("18","20","24","36","19"),
+                    Designation=c("clerk","CEO","manager","ASSISTANT","Exective"),
+                    SSN=c("121-11-2435","200-88-4758","222-10-2438","666-45-7854","111-00-1222")
+                    )
+print("Details of the employees")
+print(Employees)
