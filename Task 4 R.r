6723549e41fb74b8de17d8c95cd0d78623f8c3df
AaronBarnes21 <98524443+AaronBarnes21@users.noreply.github.com>
2022-03-31 10:14:01 +0100

    Delete Task 4 R.r

--- Task 4 R.r
+++
@@ -1,8 +0,0 @@
-marks =c(60,75,96,85,95)
-barplot(marks,
-main ="Comparing marks of 5 subjects",
-xlab ="Marks"
-ylab ="Subject"
-name.arg =c("ART","Maths","Science","PE","Sociology"),
-col = "red",
-horiz = FALSE)
