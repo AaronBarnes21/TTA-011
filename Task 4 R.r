d84ea40a179e400eb58bf484b2b28122452c8064
AaronBarnes21 <98524443+AaronBarnes21@users.noreply.github.com>
2022-03-31 10:14:40 +0100

    Add files via upload

---
+++ Task 4 R.r
@@ -0,0 +1,8 @@
+marks =c(60,75,96,85,95)
+barplot(marks,
+main ="Comparing marks of 5 subjects",
+xlab ="Marks"
+ylab ="Subject"
+name.arg =c("ART","Maths","Science","PE","Sociology"),
+col = "red",
+horiz = TRUE)
